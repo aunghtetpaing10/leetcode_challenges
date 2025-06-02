001ad43b9a2ae8775de06314f767b23b64eb872d
Aung Htet Paing <aunghtetpaing211199@gmail.com>
2025-06-02 21:29:21 +0700

    Fix syntax error

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,6 +1,7 @@
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        if strs == []: return ""
+        if strs == []: 
+            return ""
         for i in range(len(strs[0])):
             for j in range(1, len(strs)):
                 if i >= len(strs[j]) or strs[j][i] != strs[0][i]:
